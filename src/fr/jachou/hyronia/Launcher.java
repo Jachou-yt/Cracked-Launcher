c9165a909f461be96bcda23879a153bb2b413ae7
Jachou <matisse.martayan@gmail.com>
2022-02-02 19:34:36 +0100

    Website update
    Change website and fix natives

--- src/fr/jachou/hyronia/Launcher.java
+++ src/fr/jachou/hyronia/Launcher.java
@@ -1,40 +1,43 @@
 package fr.jachou.hyronia;
 
-
-import com.azuriom.azauth.AuthenticationException;
-import com.azuriom.azauth.AzAuthenticator;
-import fr.theshark34.openlauncherlib.launcher.*;
-import fr.theshark34.openlauncherlib.util.ErrorUtil;
+import fr.theshark34.openlauncherlib.launcher.GameTweak;
+import fr.theshark34.openlauncherlib.launcher.GameType;
+import club.minnced.discord.rpc.DiscordRichPresence;
+import club.minnced.discord.rpc.DiscordEventHandlers;
+import club.minnced.discord.rpc.DiscordRPC;
+import fr.theshark34.openlauncherlib.launcher.GameLauncher;
+import fr.theshark34.swinger.Swinger;
 import fr.theshark34.supdate.BarAPI;
 import fr.theshark34.supdate.SUpdate;
-import fr.theshark34.supdate.application.integrated.FileDeleter;
-import fr.theshark34.swinger.Swinger;
-
-import java.io.File;
 import java.io.IOException;
+import com.azuriom.azauth.AuthenticationException;
+import com.azuriom.azauth.AzAuthenticator;
+import fr.theshark34.openlauncherlib.util.ErrorUtil;
+import fr.theshark34.openlauncherlib.launcher.AuthInfos;
+import java.io.File;
+import fr.theshark34.openlauncherlib.launcher.GameFolder;
+import fr.theshark34.openlauncherlib.launcher.GameInfos;
+import fr.theshark34.openlauncherlib.launcher.GameVersion;
 
-
-public class Launcher {
-
-    public static final GameVersion HL_VERSION = new GameVersion("1.16.5", GameType.V1_8_HIGHER);
-    public static final GameInfos HL_INFOS = new GameInfos("hyronia", HL_VERSION, false, new GameTweak[] {});
-    public static final File HL_DIR = HL_INFOS.getGameDir();
-    public static final File HL_CRASHES_DIR = new File(HL_DIR, "crashes");
-
+public class Launcher
+{
+    public static final GameVersion HL_VERSION;
+    public static final GameInfos HL_INFOS;
+    public static final GameFolder HL_FOLDER;
+    public static final File HL_DIR;
+    public static final File HL_CRASHES_DIR;
     private static Thread updateThread;
     private static AuthInfos authInfos;
+    private static ErrorUtil errorUtil;
 
-    private static ErrorUtil errorUtil = new ErrorUtil(HL_CRASHES_DIR);
-
-    public static void auth(String username, String password) throws AuthenticationException, IOException {
-        AzAuthenticator authenticator = new AzAuthenticator("https://jachou.alwaysdata.net");
-        authInfos = authenticator.authenticate(username, password, AuthInfos.class);
+    public static void auth(final String username, final String password) throws AuthenticationException, IOException {
+        final AzAuthenticator authenticator = new AzAuthenticator("https://hyronia.evohebergweb.eu");
+        Launcher.authInfos = authenticator.authenticate(username, password, AuthInfos.class);
     }
 
     public static void update() throws Exception {
-        SUpdate su = new SUpdate("https://hyroniamc.evohebergweb.eu/S-updater/", HL_DIR);
-        su.addApplication(new FileDeleter());
-        updateThread = new Thread() {
+        final SUpdate su = new SUpdate("https://hyronia.evohebergweb.eu/S-updater/", Launcher.HL_DIR);
+        (Launcher.updateThread = new Thread() {
             private int val;
             private int max;
 
@@ -42,46 +45,88 @@ public class Launcher {
             public void run() {
                 while (!this.isInterrupted()) {
                     if (BarAPI.getNumberOfFileToDownload() == 0) {
-                        LauncherFrame.getInstance().getLauncherPanel().setInfoText("Vérification des fichiers");
+                        LauncherFrame.getInstance().getLauncherPanel().setInfoText("V\u00e9rification des fichiers");
                     }
-
-                    val = (int) (BarAPI.getNumberOfTotalDownloadedBytes() / 1000);
-                    max = (int) (BarAPI.getNumberOfTotalBytesToDownload() / 1000);
-
-                    LauncherFrame.getInstance().getLauncherPanel().getProgressBar().setMaximum(max);
-                    LauncherFrame.getInstance().getLauncherPanel().getProgressBar().setValue(val);
-
-                    LauncherFrame.getInstance().getLauncherPanel().setInfoText("Téléchargement des fichiers " +
-                            BarAPI.getNumberOfDownloadedFiles() + "/" + BarAPI.getNumberOfFileToDownload() + " " +
-                            Swinger.percentage(val, max) + "%");
+                    this.val = (int)(BarAPI.getNumberOfTotalDownloadedBytes() / 1000L);
+                    this.max = (int)(BarAPI.getNumberOfTotalBytesToDownload() / 1000L);
+                    LauncherFrame.getInstance().getLauncherPanel().getProgressBar().setMaximum(this.max);
+                    LauncherFrame.getInstance().getLauncherPanel().getProgressBar().setValue(this.val);
+                    LauncherFrame.getInstance().getLauncherPanel().setInfoText("T\u00e9l\u00e9chargement des fichiers " + BarAPI.getNumberOfDownloadedFiles() + "/" + BarAPI.getNumberOfFileToDownload() + " " + Swinger.percentage(this.val, this.max) + "%");
                 }
             }
-        };
-        updateThread.start();
+        }).start();
         su.start();
-        updateThread.interrupt();
+        Launcher.updateThread.interrupt();
+    }
+
+    public static void update_natives() throws Exception {
+        final SUpdate su = new SUpdate("http://martayan.alwaysdata.net/natives/", Launcher.HL_DIR);
+        (Launcher.updateThread = new Thread() {
+            private int val;
+            private int max;
+
+            @Override
+            public void run() {
+                while (!this.isInterrupted()) {
+                    if (BarAPI.getNumberOfFileToDownload() == 0) {
+                        LauncherFrame.getInstance().getLauncherPanel().setInfoText("V\u00e9rification des natives");
+                    }
+                    this.val = (int)(BarAPI.getNumberOfTotalDownloadedBytes() / 1000L);
+                    this.max = (int)(BarAPI.getNumberOfTotalBytesToDownload() / 1000L);
+                    LauncherFrame.getInstance().getLauncherPanel().getProgressBar().setMaximum(this.max);
+                    LauncherFrame.getInstance().getLauncherPanel().getProgressBar().setValue(this.val);
+                    LauncherFrame.getInstance().getLauncherPanel().setInfoText("T\u00e9l\u00e9chargement des fichiers " + BarAPI.getNumberOfDownloadedFiles() + "/" + BarAPI.getNumberOfFileToDownload() + " " + Swinger.percentage(this.val, this.max) + "%");
+                }
+            }
+        }).start();
+        su.start();
+        Launcher.updateThread.interrupt();
     }
 
     public static void launch() throws IOException {
-        GameLauncher gameLauncher = new GameLauncher(HL_INFOS, GameFolder.BASIC, authInfos);
-        Process p = gameLauncher.launch();
+        final GameLauncher gameLauncher = new GameLauncher(Launcher.HL_INFOS, Launcher.HL_FOLDER, Launcher.authInfos);
+        final Process p = gameLauncher.launch();
         try {
             Thread.sleep(5000L);
-        } catch (InterruptedException e) {
         }
+        catch (InterruptedException ex) {}
         LauncherFrame.getInstance().setVisible(false);
         try {
             p.waitFor();
-        } catch (InterruptedException e) {
         }
+        catch (InterruptedException ex2) {}
         System.exit(0);
     }
 
     public static void interruptThread() {
-        updateThread.interrupt();
+        Launcher.updateThread.interrupt();
     }
 
     public static ErrorUtil getErrorUtil() {
-        return errorUtil;
+        return Launcher.errorUtil;
+    }
+
+    public static void discordRPC() {
+        final DiscordRPC discord = DiscordRPC.INSTANCE;
+        final String appID = "936024107802316831";
+        final String SteamID = "";
+        final DiscordEventHandlers handlers = new DiscordEventHandlers();
+        discord.Discord_Initialize(appID, handlers, true, SteamID);
+        final DiscordRichPresence presence = new DiscordRichPresence();
+        presence.startTimestamp = System.currentTimeMillis() / 1000L;
+        presence.largeImageKey = "hyronia_no_background";
+        presence.largeImageText = "Hyronia";
+        presence.details = "Lanceur Hyronia";
+        presence.state = "Version : 1.17.1";
+        discord.Discord_UpdatePresence(presence);
+    }
+
+    static {
+        HL_VERSION = new GameVersion("1.16.5", GameType.V1_8_HIGHER);
+        HL_INFOS = new GameInfos("hyronia", Launcher.HL_VERSION, false, new GameTweak[0]);
+        HL_FOLDER = new GameFolder("assets", "libs", "natives", "minecraft.jar");
+        HL_DIR = Launcher.HL_INFOS.getGameDir();
+        HL_CRASHES_DIR = new File(Launcher.HL_DIR, "crashes");
+        Launcher.errorUtil = new ErrorUtil(Launcher.HL_CRASHES_DIR);
     }
 }
--- src/fr/jachou/hyronia/LauncherPanel.java
+++ src/fr/jachou/hyronia/LauncherPanel.java
@@ -1,140 +1,142 @@
 package fr.jachou.hyronia;
 
-import com.azuriom.azauth.AuthenticationException;
-import fr.theshark34.swinger.Swinger;
-import fr.theshark34.swinger.colored.SColoredBar;
-import fr.theshark34.swinger.event.SwingerEvent;
-import fr.theshark34.swinger.event.SwingerEventListener;
-import fr.theshark34.swinger.textured.STexturedButton;
-
-import javax.swing.*;
-import java.awt.*;
 import java.io.IOException;
+import com.azuriom.azauth.AuthenticationException;
+import javax.swing.JOptionPane;
+import fr.theshark34.swinger.event.SwingerEvent;
+import java.awt.image.ImageObserver;
+import java.awt.Graphics;
+import java.awt.Component;
+import javax.swing.border.Border;
+import java.awt.Color;
+import java.awt.LayoutManager;
+import javax.swing.JPasswordField;
+import fr.theshark34.swinger.Swinger;
+import javax.swing.JLabel;
+import fr.theshark34.swinger.colored.SColoredBar;
+import fr.theshark34.swinger.textured.STexturedButton;
+import javax.swing.JTextField;
+import java.awt.Image;
+import fr.theshark34.swinger.event.SwingerEventListener;
+import javax.swing.JPanel;
 
-
-
-public class LauncherPanel extends JPanel implements SwingerEventListener {
-
-    private Image background = Swinger.getResource("background.png");
-
-    private JTextField usernameField = new JTextField();
-    private JTextField passwordField = new JPasswordField();
-
-    private STexturedButton playButton = new STexturedButton(Swinger.getResource("play.png"));
-    private STexturedButton quitButton = new STexturedButton(Swinger.getResource("quit1.png"));
-    private STexturedButton hideButton = new STexturedButton(Swinger.getResource("quit.png"));
-
-    private SColoredBar progressBar = new SColoredBar(Swinger.getTransparentWhite(100), Swinger.getTransparentWhite(175));
-    private JLabel infoLabel = new JLabel("Cliquez sur Jouer !");
+public class LauncherPanel extends JPanel implements SwingerEventListener
+{
+    private Image background;
+    private JTextField usernameField;
+    private JTextField passwordField;
+    private STexturedButton playButton;
+    private STexturedButton quitButton;
+    private STexturedButton hideButton;
+    private SColoredBar progressBar;
+    private JLabel infoLabel;
 
     public LauncherPanel() {
+        this.background = Swinger.getResource("background.png");
+        this.usernameField = new JTextField();
+        this.passwordField = new JPasswordField();
+        this.playButton = new STexturedButton(Swinger.getResource("play.png"));
+        this.quitButton = new STexturedButton(Swinger.getResource("quit1.png"));
+        this.hideButton = new STexturedButton(Swinger.getResource("quit.png"));
+        this.progressBar = new SColoredBar(Swinger.getTransparentWhite(100), Swinger.getTransparentWhite(175));
+        this.infoLabel = new JLabel("Cliquez sur Jouer !");
         this.setLayout(null);
-
-        usernameField.setForeground(Color.WHITE);
-        usernameField.setFont(usernameField.getFont().deriveFont(20F));
-        usernameField.setCaretColor(Color.WHITE);
-        usernameField.setOpaque(false);
-        usernameField.setBorder(null);
-        usernameField.setBounds(80, 430, 390, 40);
-        this.add(usernameField);
-
-        passwordField.setForeground(Color.WHITE);
-        passwordField.setFont(passwordField.getFont().deriveFont(20F));
-        passwordField.setCaretColor(Color.WHITE);
-        passwordField.setOpaque(false);
-        passwordField.setBorder(null);
-        passwordField.setBounds(80, 566, 390, 40);
-        this.add(passwordField);
-
-        playButton.setBounds(134, 640, 262, 58);
-        playButton.addEventListener(this);
-        this.add(playButton);
-
-        quitButton.setBounds(470, 0, 79, 62);
-        quitButton.addEventListener(this);
-        this.add(quitButton);
-
-        hideButton.setBounds(420, 0, 79, 62);
-        hideButton.addEventListener(this);
-        this.add(hideButton);
-
-        progressBar.setBounds(18, 350, 510, 20);
-        this.add(progressBar);
-
-        infoLabel.setBounds(190, 320, 200, 25);
-        infoLabel.setFont(usernameField.getFont());
-        infoLabel.setForeground(Color.WHITE);
-        this.add(infoLabel);
+        this.usernameField.setForeground(Color.WHITE);
+        this.usernameField.setFont(this.usernameField.getFont().deriveFont(20.0f));
+        this.usernameField.setCaretColor(Color.WHITE);
+        this.usernameField.setOpaque(false);
+        this.usernameField.setBorder(null);
+        this.usernameField.setBounds(80, 430, 390, 40);
+        this.add(this.usernameField);
+        this.passwordField.setForeground(Color.WHITE);
+        this.passwordField.setFont(this.passwordField.getFont().deriveFont(20.0f));
+        this.passwordField.setCaretColor(Color.WHITE);
+        this.passwordField.setOpaque(false);
+        this.passwordField.setBorder(null);
+        this.passwordField.setBounds(80, 566, 390, 40);
+        this.add(this.passwordField);
+        this.playButton.setBounds(134, 640, 262, 58);
+        this.playButton.addEventListener(this);
+        this.add(this.playButton);
+        this.quitButton.setBounds(470, 0, 79, 62);
+        this.quitButton.addEventListener(this);
+        this.add(this.quitButton);
+        this.hideButton.setBounds(420, 0, 79, 62);
+        this.hideButton.addEventListener(this);
+        this.add(this.hideButton);
+        this.progressBar.setBounds(18, 350, 510, 20);
+        this.add(this.progressBar);
+        this.infoLabel.setBounds(190, 320, 200, 25);
+        this.infoLabel.setFont(this.usernameField.getFont());
+        this.infoLabel.setForeground(Color.WHITE);
+        this.add(this.infoLabel);
     }
 
-    @Override
-    public void paintComponent (Graphics g) {
+    public void paintComponent(final Graphics g) {
         super.paintComponent(g);
-
-        g.drawImage(background, 0, 0, this.getWidth(), this.getHeight(), this);
+        g.drawImage(this.background, 0, 0, this.getWidth(), this.getHeight(), this);
     }
 
     @Override
-    public void onEvent(SwingerEvent e) {
-        if (e.getSource() == playButton) {
-            setFieldsEnable(false);
-
-            if (usernameField.getText().replaceAll(" ", "").length() == 0 || passwordField.getText().length() == 0) {
-                JOptionPane.showMessageDialog(this, "Erreur veuillez entrez un pseudo et un mot de passe valides.", "Erreur", JOptionPane.ERROR_MESSAGE);
-                setFieldsEnable(true);
+    public void onEvent(final SwingerEvent e) {
+        if (e.getSource() == this.playButton) {
+            this.setFieldsEnable(false);
+            if (this.usernameField.getText().replaceAll(" ", "").length() == 0 || this.passwordField.getText().length() == 0) {
+                JOptionPane.showMessageDialog(this, "Erreur veuillez entrez un pseudo et un mot de passe valides.", "Erreur", 0);
+                this.setFieldsEnable(true);
                 return;
             }
-
-
-            Thread t = new Thread() {
-              public void run() {
-                  try {
-                      Launcher.auth(usernameField.getText(), passwordField.getText());
-                  } catch (AuthenticationException | IOException e) {
-                      JOptionPane.showMessageDialog(LauncherPanel.this, "Erreur, le mail ou le mot de passe n'est pas valide. " + e.getMessage(), "Erreur", JOptionPane.ERROR_MESSAGE);
-                      setFieldsEnable(true);
-                      return;
-                  }
-
-                  try {
-                      Launcher.update();
-                  } catch (Exception e) {
-                      Launcher.interruptThread();
-                      Launcher.getErrorUtil().catchError(e, "Impossible de mettre à jour Hyronia.");
-                      setFieldsEnable(true);
-                      return;
-                  }
-
-                  try {
-                      Launcher.launch();
-                  } catch (IOException e) {
-                      Launcher.getErrorUtil().catchError(e, "Impossible de lancer Hyronia.");
-                      setFieldsEnable(true);
-                  }
-
-                  System.out.println("Connexion réussi");
-              }
+            final Thread t = new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        Launcher.auth(LauncherPanel.this.usernameField.getText(), LauncherPanel.this.passwordField.getText());
+                    }
+                    catch (AuthenticationException | IOException authenticationException) {
+                        JOptionPane.showMessageDialog(LauncherPanel.this, "Erreur, le mail ou le mot de passe n'est pas valide. " + authenticationException.getMessage(), "Erreur", 0);
+                        LauncherPanel.this.setFieldsEnable(true);
+                        return;
+                    }
+                    try {
+                        Launcher.update();
+                    }
+                    catch (Exception e) {
+                        Launcher.interruptThread();
+                        Launcher.getErrorUtil().catchError(e, "Impossible de mettre \u00e0 jour Hyronia.");
+                        LauncherPanel.this.setFieldsEnable(true);
+                        return;
+                    }
+                    try {
+                        Launcher.launch();
+                    }
+                    catch (IOException e2) {
+                        Launcher.getErrorUtil().catchError(e2, "Impossible de lancer Hyronia.");
+                        LauncherPanel.this.setFieldsEnable(true);
+                    }
+                    System.out.println("Connexion r\u00e9ussi");
+                }
             };
             t.start();
-        } else if (e.getSource() == quitButton) {
+        }
+        else if (e.getSource() == this.quitButton) {
             System.exit(0);
-        } else if (e.getSource() == hideButton) {
-            LauncherFrame.getInstance().setState(JFrame.ICONIFIED);
+        }
+        else if (e.getSource() == this.hideButton) {
+            LauncherFrame.getInstance().setState(1);
         }
     }
 
-    private void setFieldsEnable(boolean enable) {
-        usernameField.setEnabled(enable);
-        passwordField.setEnabled(enable);
-        playButton.setEnabled(enable);
+    private void setFieldsEnable(final boolean enable) {
+        this.usernameField.setEnabled(enable);
+        this.passwordField.setEnabled(enable);
+        this.playButton.setEnabled(enable);
     }
 
     public SColoredBar getProgressBar() {
-        return progressBar;
+        return this.progressBar;
     }
 
-    public void setInfoText(String text) {
-        infoLabel.setText(text);
+    public void setInfoText(final String text) {
+        this.infoLabel.setText(text);
     }
-
 }
